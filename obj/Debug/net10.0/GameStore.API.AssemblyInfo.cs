6bbbd2766231cbaae5d37c80930bf9fd8c07da0c
TharsikanV <108121067+TharsikanV@users.noreply.github.com>
2026-07-06 12:37:07 +0530

    Add endpoints

--- obj/Debug/net10.0/GameStore.API.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9037b1014a718687c26e8dd73edec803e2ab51fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
