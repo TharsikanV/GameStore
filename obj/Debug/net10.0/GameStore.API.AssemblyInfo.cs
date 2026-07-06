944344477e616222a85df60cac49deea693e6789
TharsikanV <108121067+TharsikanV@users.noreply.github.com>
2026-07-06 15:42:04 +0530

    Add Auto migrations

--- obj/Debug/net10.0/GameStore.API.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbd8b83afecad7da7bc0e005d89b2394ae3b5d0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a870a8468a11053f3e4c621dfed2824911715ab2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
